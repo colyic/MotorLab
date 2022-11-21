1ad4fd0a7f63ecf9570746e38c036d6a7e4f7d12
colyi <colyichen82@gmail.com>
2022-11-21 17:46:14 -0500

    never gonna give you up

--- src/main/java/com/stuypulse/robot/commands/DriveFunctions.java
+++ src/main/java/com/stuypulse/robot/commands/DriveFunctions.java
@@ -3,23 +3,71 @@ package com.stuypulse.robot.commands;
 import com.stuypulse.robot.util.Motor;
 
 public class DriveFunctions {
-    
-    public static void driveForwards(Motor left, Motor right) {}
+    static double target_distance = 120;
 
-    public static void driveBackwards(Motor left, Motor right) {}
+    public static void driveForwards(Motor left, Motor right) {
+        left.set(1.0);
+        right.set(1.0);
+    }
 
-    public static void turnRight(Motor left, Motor right) {}
+    public static void driveBackwards(Motor left, Motor right) {
+        left.set(-1.0);
+        right.set(-1.0);
+    }
 
-    public static void turnLeft(Motor left, Motor right) {}
+    public static void turnRight(Motor left, Motor right) {
+        left.set(1.0);
+    }
 
-    public static void arcRight(Motor left, Motor right) {}
+    public static void turnLeft(Motor left, Motor right) {
+        right.set(1.0);
+    }
 
-    public static void arcLeft(Motor left, Motor right) {}
+    public static void arcRight(Motor left, Motor right) {
+        left.set(1.0);
+        right.set(0.5);
+    }
 
-    public static void stopDistance(Motor left, Motor right) {}
+    public static void arcLeft(Motor left, Motor right) {
+        right.set(1.0);
+        left.set(0.5);
+    }
 
-    public static void bangBang(Motor left, Motor right) {}
+    public static void stopDistance(Motor left, Motor right) {
+        if (left.getDistance() < target_distance) {
+            left.set(1);
+            right.set(1);
+        } else {
+            left.set(0);
+            right.set(0);
+        }
+    }
 
-    public static void betterControl(Motor left, Motor right) {}
+    public static void bangBang(Motor left, Motor right) {
+        if (left.getDistance() > target_distance) {
+            double original_distance = left.getDistance();
+
+            left.set(-1);
+            right.set(-1);
+
+            if (Math.abs(left.getDistance() - (original_distance + original_distance - 24)) < 1) {
+                left.set(0);
+                right.set(0);
+            }
+        } else {
+            left.set(1);
+            right.set(1);
+
+            if (Math.abs(left.getDistance() - target_distance) < 1) {
+                left.set(0);
+                right.set(0);
+            }
+        }
+    }
+
+    public static void betterControl(Motor left, Motor right) {
+        left.set((target_distance - left.getDistance()) / target_distance);
+        right.set((target_distance - right.getDistance()) / target_distance);
+    }
 
 }
